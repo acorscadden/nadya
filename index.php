3401a582c7c0ef3c0aab35f61d62a32c97ac44da
Adrian Corscadden <a.corscadden@gmail.com>
2013-01-01 14:34:19 -0800

    new cover image and moved demo reel to gallery

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
       <h3>Singer ~ Dancer ~ Actress</h3>
       <img class="home" src="./images/home.jpg" />
       <br>
-      <h2>Demo Reel</h2>
-          <iframe class="video" src="http://player.vimeo.com/video/34646642?title=0&amp;byline=0&amp;portrait=0" width="601" height="338" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 
 	</div>
 </div>
