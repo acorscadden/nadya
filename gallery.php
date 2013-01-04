627062cf68dce63926b223ccb2f72f3fdde0830a
Adrian Corscadden <a.corscadden@gmail.com>
2013-01-04 14:34:31 -0800

    new images

--- gallery.php
+++ gallery.php
@@ -3,14 +3,42 @@
 
 <div class="image_container">
   <div class = "img">
-    <a href = "images/img10_b.jpg">
-      <img src = "images/img10.jpg" />
+    <a href = "images/8.jpg">
+      <img src = "images/8s.jpg" />
+    </a>
+  </div>
+  
+  <div class = "img">
+    <a href = "images/10.jpg">
+      <img src ="images/10s.jpg" />
+    </a>
+  </div>
+
+</div>
+<div class="image_container">
+  <div class = "img">
+    <a href = "images/7.jpg">
+      <img src = "images/7s.jpg" />
+    </a>
+  </div>
+  
+  <div class = "img">
+    <a href = "images/9.jpg">
+      <img src ="images/9s.jpg" />
+    </a>
+  </div>
+</div>
+
+<div class="image_container">
+  <div class = "img">
+    <a href = "images/1.jpg">
+      <img src = "images/1s.jpg" />
     </a>
   </div>
 
   <div class = "img">
-  	<a href = "images/img2_b.jpg">
-  		<img src = "images/img2.jpg" />
+  	<a href = "images/2.jpg">
+  		<img src = "images/2s.jpg" />
   	</a>
   </div>
 </div>
@@ -20,14 +48,14 @@
 
 <div class="image_container">
   <div class = "img">
-    <a href = "images/img5_b.jpg">
-      <img src = "images/img5.jpg" />
+    <a href = "images/3.jpg">
+      <img src = "images/3s.jpg" />
     </a>
   </div>
 
   <div class = "img">
-    <a href = "images/img9_b.jpg">
-      <img src = "images/img9.jpg" />
+    <a href = "images/4.jpg">
+      <img src = "images/4s.jpg" />
     </a>
   </div>
 </div>
@@ -37,45 +65,52 @@
 
 <div class="image_container">
   <div class = "img">
-    <a href = "images/img7_b.jpg">
-      <img src = "images/img7.jpg" />
+    <a href = "images/5.jpg">
+      <img src = "images/5s.jpg" />
     </a>
   </div>
   <div class = "img">
-    <a href = "images/img8_b.jpg">
-      <img src = "images/img8.jpg" />
+    <a href = "images/6.jpg">
+      <img src = "images/6s.jpg" />
     </a>
   </div>
 </div>
 
 <div class="image_container">
   <div class = "img">
-    <a href = "images/performances/critique_pic_1.jpg">
-      <img src = "images/performances/critique_pic_1_s.png" />
+    <a href = "images/11.jpg">
+      <img src = "images/11s.jpg" />
     </a>
   </div>
   
   <div class = "img">
-    <a href = "images/performances/dance_critique_2.png">
-      <img src ="images/performances/dance_critique_2.png" />
+    <a href = "images/15.jpg">
+      <img src ="images/15s.jpg" />
     </a>
   </div>
-
 </div>
+
 <div class="image_container">
   <div class = "img">
-    <a href = "images/performances/splits.png">
-      <img src = "images/performances/splits.png" />
+    <a href = "images/13.jpg">
+      <img src = "images/13s.jpg" />
     </a>
   </div>
   
   <div class = "img">
-    <a href = "images/performances/dance_critique_1.png">
-      <img src ="images/performances/dance_critique_1_s.png" />
+    <a href = "images/14.jpg">
+      <img src ="images/14s.jpg" />
     </a>
   </div>
 </div>
 
+<div class="image_container">
+  <div class = "img">
+    <a href = "images/12.jpg">
+      <img src = "images/12s.jpg" />
+    </a>
+  </div>
+</div>
       <h2>Demo Reel</h2>
           <iframe class="video" src="http://player.vimeo.com/video/34646642?title=0&amp;byline=0&amp;portrait=0" width="601" height="338" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 <?php include('footer.php'); ?>
